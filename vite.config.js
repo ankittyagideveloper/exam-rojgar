00821db82a3b0be167d5a75629e9e7bb97508886
Ankit Tyagi <ankit.tyagi1@ibm.com>
2025-08-20 21:27:59 +0530

    added sidebar

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://examrojgar-v1.onrender.com",
+        target: "https://exam-rojgar.netlify.app",
         changeOrigin: true,
         secure: false,
         // rewrite: (path) => path.replace(/^\/api/, ''),
