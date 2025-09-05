c90743f27c9de25878555e20035485c3530576be
Ankit Tyagi <ankit.tyagi1@ibm.com>
2025-09-05 07:54:23 +0530

    style fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -35,7 +35,7 @@ function HomePage() {
               </p>
             </div>
           ) : (
-            <div className="mb-8 ">
+            <div className="hidden md:block mb-8 ">
               <h2 className="text-3xl font-bold text-gray-800 mb-2">
                 <span className="text-teal-600">ExamRojgar</span>
               </h2>
