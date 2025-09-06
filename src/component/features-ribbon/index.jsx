91cc8226c40594c5356b86506e184791480ddd2c
Ankit Tyagi <ankit.tyagi1@ibm.com>
2025-09-06 10:14:13 +0530

    chore: add style fix

--- src/component/features-ribbon/index.jsx
+++ src/component/features-ribbon/index.jsx
@@ -240,7 +240,7 @@ export default function FeaturesRibbon() {
       //   className="mx-auto w-full max-w-6xl px-3 sm:px-4"
       className="py-4"
     >
-      <div className="rounded-2xl bg-white shadow-none ring-1 ring-slate-200 ">
+      <div className="md:rounded-2xl bg-white shadow-none ring-1 ring-slate-200 ">
         <ul
           role="list"
           className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-6 "
