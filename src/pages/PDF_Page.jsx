553220e1d15d6529aac2a40ecfd10aba4a518a03
Ankit Tyagi <ankit.tyagi1@ibm.com>
2025-11-14 19:55:19 +0530

    chore: lets test

--- src/pages/PDF_Page.jsx
+++ src/pages/PDF_Page.jsx
@@ -1,40 +1,8 @@
 import React from "react";
 import { useState } from "react";
-import { Document, Page } from "react-pdf";
-
-import { PDFViewer } from "@react-pdf/renderer";
 
 const PDF_Page = () => {
-  const [numPages, setNumPages] = useState();
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess(numPages) {
-    setNumPages(numPages);
-  }
   return (
-    // <PDFViewer width="100%" height="600">
-    //   <Document
-    //     file="https://cdn.jsdelivr.net/gh/ankittyagideveloper/first-cdn-test/second-cdn.pdf"
-    //     onLoadSuccess={onDocumentLoadSuccess}
-    //   >
-    //     <Page pageNumber={pageNumber} />
-    //   </Document>
-    //   <p>
-    //     Page {pageNumber} of {numPages}
-    //   </p>
-    //   <button
-    //     onClick={() => setPageNumber((prevPage) => Math.max(prevPage - 1, 1))}
-    //   >
-    //     Prev
-    //   </button>
-    //   <button
-    //     onClick={() =>
-    //       setPageNumber((prevPage) => Math.min(prevPage + 1, numPages))
-    //     }
-    //   >
-    //     Next
-    //   </button>
-    // </PDFViewer>
     <a
       target="_blank"
       href="https://cdn.jsdelivr.net/gh/ankittyagideveloper/first-cdn-test/second-cdn.pdf"
