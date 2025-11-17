a3f5e22645746f465d6e8a727da0d89e68c5a41e
Ankit Tyagi <ankit.tyagi1@ibm.com>
2025-11-17 22:19:04 +0530

    chore: add home default route

---
+++ manifest.js
@@ -0,0 +1,71 @@
+export const manifestForPlugIn = {
+  registerType: "prompt",
+  includeAssests: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "React-vite-app",
+    short_name: "react-vite-app",
+    description: "I am a simple vite app",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        purpose: "maskable",
+        sizes: "274.2857142857143x274.2857142857143",
+        src: "maskable_icon.png",
+        type: "image/png",
+      },
+      {
+        purpose: "maskable",
+        sizes: "48x48",
+        src: "maskable_icon_x48.png",
+        type: "image/png",
+      },
+      {
+        purpose: "maskable",
+        sizes: "72x72",
+        src: "maskable_icon_x72.png",
+        type: "image/png",
+      },
+      {
+        purpose: "maskable",
+        sizes: "96x96",
+        src: "maskable_icon_x96.png",
+        type: "image/png",
+      },
+      {
+        purpose: "maskable",
+        sizes: "128x128",
+        src: "maskable_icon_x128.png",
+        type: "image/png",
+      },
+      {
+        purpose: "maskable",
+        sizes: "192x192",
+        src: "maskable_icon_x192.png",
+        type: "image/png",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#f0e7db",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
+import { manifestForPlugIn } from "./manifest";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA(manifestForPlugIn)],
   resolve: {
     alias: {
       "@": path.resolve("./src"),
