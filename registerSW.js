4d0e2ff493ae68dce7160f28d43524dfba1f3914
Ankit Tyagi <ankittyagideveloper.com>
2026-07-02 22:13:38 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/exam-rojgar/sw.js', { scope: '/exam-rojgar/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
